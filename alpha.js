fdd4efaf520142dcdc04b4aa6437fb3236bdf0b1
Santhosh-Bosku <santhoshbablu1997@gmail.com>
2021-03-21 12:31:22 +0530

    no js

--- alpha.js
+++ alpha.js
@@ -1,8 +1,12 @@
-window.onscroll = function(){
-    var top = window.scrollY;
-    if(top >= 10){
-        document.body.requestFullscreen()
-    }else{
-        document.body.webkitExitFullscrenn()
-    }
-}
+// window.onscroll = function(){
+//     var top = window.scrollY;
+//     if(top >= 10){
+//         document.body.requestFullscreen()
+//     }else{
+//         document.body.webkitExitFullscrenn()
+//     }
+// }
+
+// if(document.getElementById(in-comment).checked==true)
+// {
+// }
