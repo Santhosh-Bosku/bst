322dd00e06d4b3b98a19fa9a13a662d3e1e691f4
Santhosh-Bosku <santhoshbablu1997@gmail.com>
2021-05-02 18:20:54 +0530

    done

--- User-dashboard/demo.js
+++ User-dashboard/demo.js
@@ -1,5 +1,20 @@
+function readURL(input) {
+  document.getElementById("myImg").style.display="block";
+  if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          $('#myImg')
+              .attr('src', e.target.result);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+  }
+}
+
 function chapterNumber()
 {
     val = document.getElementById("select1").value;
     document.getElementById("select2").setAttribute('list',val)
 }
+
