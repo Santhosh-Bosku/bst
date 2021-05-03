6e65c631626b58521e10a78b78b89f50d9671fc5
Santhosh-Bosku <santhoshbablu1997@gmail.com>
2021-05-03 14:05:10 +0530

    done

---
+++ style/script.js
@@ -0,0 +1,24 @@
+header = document.querySelector('header');
+header = document.querySelector('header');
+window.onscroll = function scrolled()
+{
+    if(window.scrollY >= 50)
+    {
+        document.getElementById('header').classList.add("scrolled");
+        document.getElementById('logoh1').classList.add("h1-scrolled");
+        document.querySelectorAll('nav a').forEach((link) =>{
+            link.classList.add('h1-scrolled')
+        })
+    }
+    else if(window.scrollY < 50)
+    {
+        document.getElementById('header').classList.remove("scrolled");
+        document.getElementById('logoh1').classList.remove("h1-scrolled");
+        document.querySelectorAll('nav a').forEach((link) =>{
+            link.classList.remove('h1-scrolled')
+        })
+    }
+}
+
+
+
