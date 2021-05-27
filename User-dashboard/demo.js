24ca604ac271d3dfeee4b8149d1269f49285bb91
Santhosh-Bosku <78033233+Santhosh-Bosku@users.noreply.github.com>
2021-05-27 17:34:32 +0530

    Add files via upload

---
+++ User-dashboard/demo.js
@@ -0,0 +1,43 @@
+function readURL(input) {
+  document.getElementById("myImg").style.display="block";
+  if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          $('#myImg')
+              .attr('src', e.target.result);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function chapterNumber()
+{
+    val = document.getElementById("select1").value;
+    document.getElementById("select2").setAttribute('list',val)
+}
+
+function clicked(clk)
+{
+    // const label = document.this.id;
+    console.log(clk);
+    // alert("label");
+}
+
+function show()
+{
+    document.querySelector('#setting menu')
+}
+
+function itr()
+{
+    n=1
+    for(i = 0; i<n ;i++)
+    {
+        n++;
+        console.log(i);
+
+    }
+}
+
